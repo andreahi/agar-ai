40c625828c37f9782fd593e0e825c7ba707cabb8
Andreas Iversen <Andreas-Hoie.Iversen@telenor.com>
2019-04-22 17:09:45 +0200

    Don't load model on startup if not present

--- run_model.py
+++ run_model.py
@@ -10,8 +10,12 @@ import numpy as np
 from tensorflow.python.saved_model import tag_constants
 
 last_reload = datetime.datetime.now()
+
+run_without_model = not os.path.isdir('model');
 with tf.Session(graph=tf.Graph()) as sess:
-    tf.saved_model.loader.load(sess, [tag_constants.SERVING], "model")
+
+    if not run_without_model:
+        tf.saved_model.loader.load(sess, [tag_constants.SERVING], "model")
 
     graph = tf.get_default_graph()
 
@@ -28,14 +32,17 @@ with tf.Session(graph=tf.Graph()) as sess:
         x = data["food"]
         individual_values = data["individual_values"]
         print(individual_values)
-        actions = sess.run(['action_pred:0'],
-                 feed_dict={'food:0': np.array([x], dtype=float), 'individual_values:0': np.array([individual_values], dtype=float)})[0][0]
 
-        actions = actions.tolist()
-        if random.randint(0, 100) > 80:
+        if not run_without_model:
+            actions = sess.run(['action_pred:0'],
+                     feed_dict={'food:0': np.array([x], dtype=float), 'individual_values:0': np.array([individual_values], dtype=float)})[0][0]
+
+            actions = actions.tolist()
+            if random.randint(0, 100) > 80:
+                actions = [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)]
+        else:
             actions = [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)]
 
-        #actions = [random.randint(0, 10) -5, random.randint(0, 10) -5, random.randint(0, 10) -5, random.randint(0, 10) -5]
         r.set("action", json.dumps(actions))
 
         if (datetime.datetime.now() - last_reload).seconds > 200 and os.path.isdir('model'):
