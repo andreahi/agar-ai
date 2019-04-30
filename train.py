2135f859ed3a9df913dd38be24bfe5f911878bb3
Andreas Iversen <Andreas-Hoie.Iversen@telenor.com>
2019-04-30 21:07:00 +0200

    step

--- train.py
+++ train.py
@@ -272,7 +272,7 @@ with tf.Session() as sess:
         shuffle_in_unison(next_x_train, next_individual_values_train, x_train, individual_values_train, reward_train, actions_train)
         print("dataset size: ", len(x_train))
         step = 1000
-        for _ in range(10000):
+        for _ in range(100):
             for i in range(0, 100000, step):
                 if i + step > len(x_train):
                     break
